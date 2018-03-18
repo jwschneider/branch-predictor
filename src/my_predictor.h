9b026d8ce14290d8f29c14516a69d4ea3089a142
Morgan Reschenberg <mreschenberg@berkeley.edu>
2018-03-18 16:43:01 -0700

    Updated predict function together.

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -20,38 +20,37 @@ const unsigned int TAG_LEN = 4;         // so that the prediction ang tag can fi
                                         // unsigned int, we use 30 bit tags
 	my_update u;
 	branch_info bi;
-  unsigned char hist;
+  	unsigned char hist;
 
 	vector<unsigned int *> pred; 
 
 	my_predictor (void) : hist(0) { 
 		/* iterate through all history and set to 0 for all entries */
 		for (unsigned int i = 0; i < HISTORY_LEN + 1; i++) {
-    	unsigned int tbl[1<<TABLE_BITS];
-    	memset (tbl, 0, sizeof (tbl));
-      pred.push_back(tbl);
+	    	unsigned int tbl[1<<TABLE_BITS];
+	    	memset (tbl, 0, sizeof (tbl));
+	    	pred.push_back(tbl);
      	} 
 	}
 
 	branch_update *predict (branch_info &b) {
 		bi = b;
 		if (b.br_flags & BR_CONDITIONAL) {
-			/* TODO: did you want this to be logical and or bitwise and? */
-	    unsigned int ct = 0;
-      u.table = 0;
-      u.index = b.address & ((1 << TAG_LEN) - 1);
+		    unsigned int tableCounter = 0;
+	    	u.table = 0;
+	    	u.index = b.address & ((1 << TAG_LEN) - 1);
 			for (vector<unsigned int *>::iterator it = pred.begin(); it != pred.end(); it++) {
 				// for each item in the history list
-        if (*(*it + ((b.address ^ (hist & ct)) & ((1<<TAG_LEN) - 1))) ==
-            (b.address & ((1<<TAG_LEN) - 1))) {
+	        	if (*(*it + ((b.address ^ (hist & ct)) & ((1<<TABLE_BITS) - 1))) & ((1<<TAG_LEN) - 1)) ==
+	            		(b.address & ((1<<TAG_LEN) - 1)) {
 					// if the item in the table matches the current item, update it
-  				u.table = ct;
-					u.index = ((b.address ^ (hist & ct)) & ((1<<TAG_LEN) - 1));
+	  				u.table = tableCounter;
+					u.index = ((b.address ^ (hist & ct)) & ((1<<TABLE_BITS) - 1));
 				}
-				// increment counter
-				ct++;
-			}
-      unsigned int *tbl = pred[u.table];
+					// increment counter
+					tableCounter++;
+				}
+	      	unsigned int *tbl = pred[u.table];
 			u.direction_prediction (*(tbl + u.index) >> (TAG_LEN + 1));
 		} else {
 			u.direction_prediction (true);                  
