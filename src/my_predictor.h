7a13948ec77f3646be90cf983acecd817082b8f9
John Schneider <johnschneider@berkeley.edu>
2018-03-18 14:59:21 -0700

    dc: stack empty bug

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -1,79 +1,48 @@
-// tagged-hybrid_predictor.h
-// The tagged hybrid predictor uses cascading prediction tables
-// indexed by a hash of the history and the pc.
-// Current hash implementation is a simple xor hash; TBU.
-#include <vector>
-using std::vector;
-
+// twobit_predictor.h
+// This file is the sample predictor shipped with the package
+// simplified to be just a two bit predictor indexed by the PC
+// Added comments for clarity
+//
 class my_update : public branch_update {
 public:
-  unsigned int index;     // index in the table that was updated
-  unsigned int table;     // which table was updated, 0 being the base
+	unsigned int index;
 };
 
 class my_predictor : public branch_predictor {
 public:
-const unsigned int TABLE_BITS	= 10;   // start with 2^10 rows
-const unsigned int HISTORY_LEN = 4;    // start with 4 bit history length, as in the
-                                        // book example  
-const unsigned int TAG_LEN = 30;       // so that the prediction ang tag can fit into an
-                                        // unsigned int, we use 30 bit tags
+#define TABLE_BITS	10      // table has 2^10 rows
 	my_update u;
 	branch_info bi;
-  unsigned char hist;
+	unsigned char tab[1<<TABLE_BITS];     // each table entry is a char
+                                        // 0 is strongly not taken, 3 is
+                                        //   strongly taken
 
-	vector<unsigned int *> pred; 
-
-	my_predictor (void) : hist(0) { 
-		/* iterate through all history and set to 0 for all entries */
-		for (unsigned int i = 0; i < HISTORY_LEN + 1; i++) {
-    	unsigned int tbl[1<<TABLE_BITS];
-    	memset (tbl, 0, sizeof (tbl));
-      pred.push_back(tbl);
-     	} 
+	my_predictor (void) { 
+		memset (tab, 0, sizeof (tab));      // initialize the table to all strongly
+                                        // not taken
 	}
 
-	branch_update *predict (branch_info &b) {
+	branch_update *predict (branch_info & b) {
 		bi = b;
 		if (b.br_flags & BR_CONDITIONAL) {
-			/* TODO: did you want this to be logical and or bitwise and? */
-	    unsigned int ct = 0;
-      u.table = 0;
-      u.index = b.address & ((1 << TAG_LEN) - 1);
-			for (vector<unsigned int *>::iterator it = pred.begin(); it != pred.end(); it++) {
-				// for each item in the history list
-        if (*(*it + ((b.address ^ (hist & ct)) & ((1<<TAG_LEN) - 1))) ==
-            (b.address & ((1<<TAG_LEN) - 1))) {
-					// if the item in the table matches the current item, update it
-					u.table = ct;
-					u.index = ((b.address ^ (hist & ct)) & ((1<<TAG_LEN) - 1));
-				}
-				// increment counter
-				ct++;
-			}
-      unsigned int *tbl = pred[u.table];
-			u.direction_prediction (*(tbl + u.index) >> (TAG_LEN + 1));
+			u.index = (b.address & ((1<<TABLE_BITS)-1));    // index the table by PC
+			u.direction_prediction (tab[u.index] >> 1);
 		} else {
-			u.direction_prediction (true);                  
+			u.direction_prediction (true);                  // take non-conditional branches
 		}
-		u.target_prediction (0);                          
+		u.target_prediction (0);                          // not doing any target prediction
 		return &u;
 	}
 
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {               
-    // TODO: implement the update functionality
-      hist = ((hist << 1) | taken) & ((1<< HISTORY_LEN) - 1); 
-      unsigned int *tbl = pred[((my_update*)u)->table];
-      unsigned int prediction = *(tbl + ((my_update*)u)->index) >> TAG_LEN;
-			if (taken) {                      
-				if (prediction < 3) prediction++;             
+			unsigned char *c = &tab[((my_update*)u)->index];
+			if (taken) {                      // i dont know why they bother with char * here,
+                                        // but theyre the c++ expert, not me
+				if (*c < 3) (*c)++;             // update the table with prediction result
 			} else {
-				if (prediction > 0) prediction--;
+				if (*c > 0) (*c)--;
 			}
-      *(tbl + ((my_update*)u)->index) = (*(tbl + ((my_update*)u)->table) &
-          ((1<<TAG_LEN) - 1)) |
-          (prediction << TAG_LEN);
 		}
 	}
 };
--- src/tagged-hybrid_predictor.h
+++ src/tagged-hybrid_predictor.h
@@ -2,7 +2,8 @@
 // The tagged hybrid predictor uses cascading prediction tables
 // indexed by a hash of the history and the pc.
 // Current hash implementation is a simple xor hash; TBU.
-#include <list>
+#include <vector>
+using std::vector;
 
 class my_update : public branch_update {
 public:
@@ -12,22 +13,23 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define TABLE_BITS	10    // start with 2^10 rows
-#define HISTORY_LEN 4     // start with 4 bit history length, as in the
-                          // book example  
-#define TAG_LEN 30        // so that the prediction ang tag can fit into an
-                          // unsigned int, we use 30 bit tags
+const unsigned int TABLE_BITS	= 10;   // start with 2^10 rows
+const unsigned int HISTORY_LEN = 4;    // start with 4 bit history length, as in the
+                                        // book example  
+const unsigned int TAG_LEN = 30;       // so that the prediction ang tag can fit into an
+                                        // unsigned int, we use 30 bit tags
 	my_update u;
 	branch_info bi;
-    unsigned char hist;
+  unsigned char hist;
 
-	std::list<unsigned int *> pred (HISTORY_LEN + 1); 
+	vector<unsigned int *> pred; 
 
 	my_predictor (void) : hist(0) { 
 		/* iterate through all history and set to 0 for all entries */
-		for (std::list<unsigned int *>::iterator it = pred.begin(); it != pred.end(); it++) {
-    	*it = new unsigned int[1<<TABLE_BITS];
-    	memset (*it, 0, 1<<TABLE_BITS);
+		for (unsigned int i = 0; i < HISTORY_LEN + 1; i++) {
+    	unsigned int tbl[1<<TABLE_BITS];
+    	memset (tbl, 0, sizeof (tbl));
+      pred.push_back(tbl);
      	} 
 	}
 
@@ -35,26 +37,22 @@ public:
 		bi = b;
 		if (b.br_flags & BR_CONDITIONAL) {
 			/* TODO: did you want this to be logical and or bitwise and? */
-	    	unsigned int ct = 0;
-			for (std::list<unsigned int *>::iterator it = pred.begin(); it != pred.end(); it++) {
+	    unsigned int ct = 0;
+      u.table = 0;
+      u.index = b.address & ((1 << TAG_LEN) - 1);
+			for (vector<unsigned int *>::iterator it = pred.begin(); it != pred.end(); it++) {
 				// for each item in the history list
-				if ((*(*it + (b.address ^ (hist & ct))) & (1<<TAG_LEN - 1)) ==
-					(b.address & (1 <<TAG_LEN - 1))) {
+        if (*(*it + ((b.address ^ (hist & ct)) & ((1<<TAG_LEN) - 1))) ==
+            (b.address & ((1<<TAG_LEN) - 1))) {
 					// if the item in the table matches the current item, update it
 					u.table = ct;
-					u.index = (b.address ^ (hist & ct));
-					u.direction_prediction (*(*it + u.index) >> TAG_LEN);
-					break;
-				} else if (it == pred.end()) {
-					// otherwise, if this is the last item
-					u.table = 0;
-					u.index = b.address;
-					u.direction_prediction (*(*l.begin() + u.index >> TAG_LEN));
-					// TODO: should this be an expression ^
+					u.index = ((b.address ^ (hist & ct)) & ((1<<TAG_LEN) - 1));
 				}
 				// increment counter
 				ct++;
 			}
+      unsigned int *tbl = pred[u.table];
+			u.direction_prediction (*(tbl + u.index) >> (TAG_LEN + 1));
 		} else {
 			u.direction_prediction (true);                  
 		}
@@ -65,11 +63,17 @@ public:
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {               
     // TODO: implement the update functionality
+      hist = ((hist << 1) | taken) & ((1<< HISTORY_LEN) - 1); 
+      unsigned int *tbl = pred[((my_update*)u)->table];
+      unsigned int prediction = *(tbl + ((my_update*)u)->index) >> TAG_LEN;
 			if (taken) {                      
-				if (*c < 3) (*c)++;             
+				if (prediction < 3) prediction++;             
 			} else {
-				if (*c > 0) (*c)--;
+				if (prediction > 0) prediction--;
 			}
+      *(tbl + ((my_update*)u)->index) = (*(tbl + ((my_update*)u)->table) &
+          ((1<<TAG_LEN) - 1)) |
+          (prediction << TAG_LEN);
 		}
 	}
 };
