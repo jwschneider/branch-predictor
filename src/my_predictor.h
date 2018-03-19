be2dbf0e6251e5e9cfa3afa20cbb00095f2a1040
Morgan Reschenberg <mreschenberg@berkeley.edu>
2018-03-18 17:29:58 -0700

    Update conditional updating of tables based on alternate and previous.

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -3,11 +3,13 @@
 // indexed by a hash of the history and the pc.
 // Current hash implementation is a simple xor hash; TBU.
 #include <vector>
+#include <stdlib.h>
 using std::vector;
 
 class my_update : public branch_update {
 public:
   unsigned int index;     // index in the table that was updated
+  unsigned int tag;
   unsigned int table;     // which table was updated, 0 being the base
 };
 
@@ -46,6 +48,7 @@ const unsigned int TAG_LEN = 4;         // so that the prediction ang tag can fi
 					// if the item in the table matches the current item, update it
 	  				u.table = tableCounter;
 					u.index = ((b.address ^ (hist & ct)) & ((1<<TABLE_BITS) - 1));
+					u.tag = b.address & ((1 << TAG_LEN) - 1);
 				}
 					// increment counter
 					tableCounter++;
@@ -61,18 +64,42 @@ const unsigned int TAG_LEN = 4;         // so that the prediction ang tag can fi
 
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {               
-    // TODO: implement the update functionality
-      hist = ((hist << 1) | taken) & ((1<< HISTORY_LEN) - 1); 
-      unsigned int *tbl = pred[((my_update*)u)->table];
-      unsigned int prediction = *(tbl + ((my_update*)u)->index) >> TAG_LEN;
-			if (taken) {                      
-				if (prediction < 3) prediction++;             
+		  	hist = ((hist << 1) | taken) & ((1<< HISTORY_LEN) - 1); 
+			unsigned int* tbl = pred[((my_update *)u)->table];
+			unsigned int prediction = *(tbl + ((my_update *)u)->index) >> (TAG_LEN);
+
+			if (taken == (prediction >> 1)) {
+			// if prediction was correct
+				if (taken) {
+					if (prediction < 3) prediction++;             
+				} else {
+					if (prediction > 0) prediction--;
+				}
+				*(tbl + ((my_update *)u)->index) = (*(tbl + ((my_update *)u)->index) &
+          			((1<<TAG_LEN) - 1)) |
+          			(prediction << TAG_LEN);
+          		if ((my_update *)u->table - 1 > 0) {
+          			// if update table has previous, update there too
+					tbl = pred[(((my_update *)u)->table) - 1];
+          			*(tbl + ((my_update *)u)->index) = (*(tbl + ((my_update *)u)->index) &
+          			((1<<TAG_LEN) - 1)) |
+          			(prediction << TAG_LEN);
+          		}
 			} else {
-				if (prediction > 0) prediction--;
+			// if prediction was incorrect, allocate space 
+				srand(1);
+				int rNum = rand()%2;
+				if (rNum) {
+					// rNum is odd, update table i + 1
+					unsigned int *table = pred[((u->table + 1) & ((1<<HISTORY_LEN) - 1))];
+					// get the table to update from pred
+					*(table + u->index) = ((taken << 1) << TAG_LEN) | (u->tag);
+					// store the updated prediction in table
+				} else {
+					// if rNum is even, update either i+2, i+3
+				}
 			}
-      *(tbl + ((my_update*)u)->index) = (*(tbl + ((my_update*)u)->table) &
-          ((1<<TAG_LEN) - 1)) |
-          (prediction << TAG_LEN);
+      
 		}
 	}
 };
