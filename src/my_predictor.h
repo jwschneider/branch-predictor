98e374e5d687e3ecddb3e52d79afaa044824bd7e
John Schneider <johnschneider@berkeley.edu>
2018-03-19 16:49:45 -0700

    Added table_count global variable

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 
 using std::vector;
 using std::cout;
@@ -19,30 +20,47 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-const unsigned int TABLE_BITS	= 8;   // start with 2^10 rows
-const unsigned int HISTORY_LEN = 31;    // start with 4 bit history length, as in the
+const unsigned int TABLE_BITS	= 12;   // start with 2^10 rows
+const unsigned int HISTORY_LEN = 32;    // start with 4 bit history length, as in the
                                         // book example  
 const unsigned int TAG_LEN = 6;         // so that the prediction ang tag can fit into an
                                         // unsigned int, we use 30 bit tags
+const unsigned int TABLE_CT = 6;
+
 	my_update u;
 	branch_info bi;
-  	unsigned char hist;
+  unsigned char hist;
 
 	vector<unsigned int *> pred; 
 
 	my_predictor (void) : hist(0) { 
 		/* iterate through all history and set to 0 for all entries */
-		for (unsigned int i = 0; i < HISTORY_LEN + 1; i++) {
+		for (unsigned int i = 0; i < TABLE_CT; i++) {
 		unsigned int* tbl = (unsigned int*) malloc((1 << TABLE_BITS) * sizeof(unsigned int));
     // what to initialize predictions to
     for (int i = 0; i < (1<<TABLE_BITS); i++) {
       *(tbl + i) = 1 << (TAG_LEN + 1);
     }
 		pred.push_back(tbl);
-     	} 
+    } 
 	}
+
   unsigned int address_hash(unsigned int address, unsigned int history) {
     return address ^ history;
+  }
+
+  // returns the i'th element of the geometric progression,
+  // or 0 if i is 0.
+  unsigned int gp(int base, int i) {
+    if (i == 0)
+      return 0;
+    else
+      return pow(base, i);
+  }
+
+  // returns the bottom i bits of hist
+  unsigned int hist_mask(int i) {
+    return hist & ((1<<i)-1);
   }
 
 	branch_update *predict (branch_info &b) {
@@ -50,13 +68,13 @@ const unsigned int TAG_LEN = 6;         // so that the prediction ang tag can fi
 		if (b.br_flags & BR_CONDITIONAL) {
 	    	u.table = 0;
 	    	u.index = b.address & ((1 << TABLE_BITS) - 1);
-			for (unsigned int tableCounter = 1; tableCounter < HISTORY_LEN + 1; tableCounter++) {
-				// for each item in the history list
-	        	if ((*(pred[tableCounter] + (address_hash(b.address, (hist & (1<<tableCounter) - 1)) & ((1<<TABLE_BITS) - 1))) & ((1<<TAG_LEN) - 1)) ==
-	            		(b.address & ((1<<TAG_LEN) - 1))) {
+			for (unsigned int tableCounter = 1; tableCounter < TABLE_CT; tableCounter++) {
+	        	if ((*(pred[tableCounter] + (address_hash(b.address, hist_mask(gp(2, tableCounter))) & ((1<<TABLE_BITS) - 1))) 
+                 & ((1<<TAG_LEN) - 1)) 
+                 ==	(b.address & ((1<<TAG_LEN) - 1))) {
 					// if the item in the table matches the current item, update it
-	  				u.table = tableCounter;
-					u.index = (address_hash(b.address, (hist & (1<<tableCounter) - 1)) & ((1<<TABLE_BITS) - 1));
+              u.table = tableCounter;
+					    u.index = (address_hash(b.address, hist_mask(gp(2, tableCounter))) & ((1<<TABLE_BITS) - 1));
 				}
 				// increment counter
 			}
@@ -119,17 +137,17 @@ const unsigned int TAG_LEN = 6;         // so that the prediction ang tag can fi
 						t_index = (y->table + 3);
 					}
 				}
-				if (t_index > HISTORY_LEN) {
+				if (t_index >= TABLE_CT) {
 					// if our adjustment has caused us to go out-of-bounds
-					if (y->table + 1 <= HISTORY_LEN) {
+					if (y->table + 1 < TABLE_CT) {
 						// if we have a one-right table to update, update that
 						t_index = y->table+1;
 					} else {
 						// otherwise, just update the last table
-						t_index = HISTORY_LEN;
+						t_index = TABLE_CT - 1;
 					}
 				}
-				*(pred[t_index] + (address_hash(bi.address, (hist & (1<<t_index)-1)) & ((1<<TABLE_BITS) - 1))) =
+				*(pred[t_index] + (address_hash(bi.address, hist_mask(gp(2, t_index))) & ((1<<TABLE_BITS) - 1))) =
 					 ((((unsigned int) taken << 1) | (1 - taken)) << TAG_LEN) | ((bi.address) & ((1<<TAG_LEN) - 1));
 				// store the updated prediction in table
 			}
