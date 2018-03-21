68235ec0a6b18d10e548cc2dbfd88d7449aadedf
John Schneider <johnschneider@berkeley.edu>
2018-03-21 11:29:33 -0700

    Stripped comments to prepare for submission

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -1,7 +1,7 @@
 // tagged-hybrid_predictor.h
 // The tagged hybrid predictor uses cascading prediction tables
 // indexed by a hash of the history and the pc.
-// Current hash implementation is a simple xor hash; TBU.
+// Current hash implementation is a simple xor hash.
 #include <vector>
 #include <stdlib.h>
 #include <iostream>
@@ -10,23 +10,6 @@
 using std::vector;
 using std::cout;
 using std::endl;
-
-// This function is for printing binary values, delete before submission
-void printBits(size_t const size, void const * const ptr)
-{
-  unsigned char *b = (unsigned char*) ptr;
-  unsigned char byte;
-  int i, j;
-  for (i=size-1;i>=0;i--)
-  {
-    for (j=7;j>=0;j--)
-    {
-      byte = (b[i] >> j) & 1;
-      printf("%u", byte);
-    }
-  }
-}
-
 
 class my_update : public branch_update {
 public:
@@ -38,23 +21,18 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-//const unsigned int LIMIT_FACTOR = 15000; // when to wipe table
-const unsigned int TABLE_BITS	= 12;     // 2^12(rows per table) * 2^3(tables) * 2^3(bits per row)
-const unsigned int HISTORY_LEN = 64;    // is 2^18 Kibits = 2^15 KiB
-const unsigned int TAG_LEN = 6;
-const unsigned int TABLE_CT = 8;
-const bool UPDATE_ALT_ON_HIT = true;    // I tested this once and it seems to work better 
-                                        // with this on
+const unsigned int TABLE_BITS	= 12;       // Number of rows in the table
+const unsigned int HISTORY_LEN = 64;      // Length of history recorded
+const unsigned int TAG_LEN = 6;           // Length of tag entries
+const unsigned int TABLE_CT = 8;          // Number of tables
+const bool UPDATE_ALT_ON_HIT = true;      // Whether we update the alternate on a providor hit
 	my_update u;
 	branch_info bi;
-	unsigned long long hist;    // I made history a long long (64 bits on most machines) 
-                              // so that we could have 8 tables
-
+	unsigned long long hist;                // Since history is capped at 64, we can fit it into a long 
 	vector<unsigned int *> pred; 
 
 
-  // Initialize the tables to a particular value
-  // Weakly not taken preforms a tiny bit better than weakly taken
+  // Initialize the tables to weakly not taken
 	my_predictor (void) : hist(0) { 
 		for (unsigned int i = 0; i < TABLE_CT; i++) {
 			unsigned int* tbl = (unsigned int*) malloc((1 << TABLE_BITS) * sizeof(unsigned int));
@@ -83,21 +61,10 @@ const bool UPDATE_ALT_ON_HIT = true;    // I tested this once and it seems to wo
       hist = (hist << 1) | taken;
   }
 
-  // This is the part that was killing us. We two instances of a branch with
-  // different histories to map to different indexes in the same table, so that
-  // both can exist in the table at the same time. xor is a fine hash function,
-  // but the problem is that we were hashing address with history and then only
-  // looking at the bottom 12 bits of it (since we have to fit in the table).
-  // That meant that even though two addresses had different histories, if they
-  // were at least the same in the bottom 12 bits, then the two instances of
-  // the branch would map to the same place in the highest table, causing
-  // a ping pong effect. What i've done below is folded the history into the
-  // address. If the size of the history that we're looking to hash is larger
-  // than the number of rows in the table, first we hash the lower bits into
-  // the address, then we slide history down and hash more bits into the
-  // address. This way if a branch has two instances that match in history even
-  // up to the 63rd bit but differ in the 64th, they will map to different
-  // indices.
+  // Hashes together the PC and some amount of the history. We use xor as
+  // the hash function. In the case that the amount of history we want to hash
+  // is larger than the bits table length, we fold the history down and hash
+  // all the bits into the space of the table length
   unsigned int table_index(unsigned int address, unsigned int table_ct) {
     unsigned int accumulator = address;
     unsigned int batch = (unsigned int) gp(table_ct);
@@ -158,62 +125,31 @@ const bool UPDATE_ALT_ON_HIT = true;    // I tested this once and it seems to wo
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) {        
 			my_update* y = (my_update*) u;
-      // These are the predictions given by the provider and alternate
       unsigned int &prov_pred = *(pred[y->prov] + table_index(bi.address, y->prov));
       unsigned int &alt_pred = *(pred[y->alt] + table_index(bi.address, y->alt));
-      // This is a really useful list of things to print when debugging
-      // Delete before submission but please dont delete until we're
-      // sure we have the correct implementation
-      // It follows the trace of one address in 164, showing how it settles
-      // itself into the table. If you're interested, run predict 164 > logfile
-      // and watch the trace
-      //  if (bi.address == 134515682) {
-         // printf("Prov:%d\tIndex: ", y->prov);
-         // unsigned int prov_index = table_index(bi.address, y->prov);
-         // printBits(sizeof(unsigned int), &prov_index);
-         // printf("\tPred: %d", prov_pred >> TAG_LEN);
-         // printf("\tAlt:%d\tIndex: ", y->alt);
-         // unsigned int alt_index = table_index(bi.address, y->alt);
-         // printBits(sizeof(unsigned int), &alt_index);
-         // printf("\tPred: %d", alt_pred >> TAG_LEN);
-         // printf("\tTaken: %d\tHist: ", taken);
-         // printBits(sizeof(hist), &hist);
-         // puts("");
-       // }
-			//printf ("%d\t%d\t%d\t%d\n", y->table, y->index, prediction, taken);
 			if (taken == y->direction_prediction()) {
+        // On a correct prediction, bump the prediction counters
         update_prediction(prov_pred, taken);
-        // We can choose to update the alternate on a hit if we want,
-        // this is something we would do if we add use bits
         if (UPDATE_ALT_ON_HIT)
           update_prediction(alt_pred, taken);
 			} else {
-        // Also useful for debugging the same trace
-        // if (bi.address == 134515682)
-          // printf("Mispredict!\n");
-        // Another design change I made was that if we miss on the last table,
-        // we do not go about allocating a new entry. Instead we just treat it
-        // like a simple two-bit predictor, bumping the count up or down
+        // On an incorrect prediction, we must allocate space in a new table
         if (y->prov < TABLE_CT - 1) {
           srand(1);
+          // Use table i+1 50% of the time, i+2 and i+3 each 25% of the time
           int rNum = rand()%2; 
           unsigned int tbl_ct = y->prov; 
           if (rNum) {
-            // rNum is odd, update table i + 1
             tbl_ct = (y->prov + 1);
           } else {
-            // if rNum is even, update either i+2, i+3
             rNum = rand()%2;
             if (rNum) {
-              // if rNUm is odd, update table i+2
               tbl_ct = (y->prov + 2);
             } else {
-              // if rNum is even, update table i+3
               tbl_ct = (y->prov + 3);
             }
           }
           if (tbl_ct >= TABLE_CT) {
-            // if our adjustment has caused us to go out-of-bounds
             tbl_ct = TABLE_CT - 1;
           }
           // We allocate a new entry in the chosen table to weakly taken
@@ -227,7 +163,6 @@ const bool UPDATE_ALT_ON_HIT = true;    // I tested this once and it seems to wo
         }
 			}
 		}
-    // Moved this function out for clarity
     advance_history(taken);
 	}
 };
